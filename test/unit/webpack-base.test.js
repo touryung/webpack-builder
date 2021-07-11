a414659b9db123a37e2ed400f2e71b66449a9c95
touryung <kreios@qq.com>
2021-07-11 17:53:04 +0800

    chore: update travis config

--- test/unit/webpack-base.test.js
+++ test/unit/webpack-base.test.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
+const baseConfig = require("../../lib/webpack.base");
 
 describe("webpack.base.js test case", () => {
-  const baseConfig = require("../../lib/webpack.base");
   it("entry", () => {
     assert.strictEqual(baseConfig.entry.index.indexOf("webpack-builder/test/template/src/index/index.js") > -1, true);
     assert.strictEqual(baseConfig.entry.search.indexOf("webpack-builder/test/template/src/search/index.js") > -1, true);
